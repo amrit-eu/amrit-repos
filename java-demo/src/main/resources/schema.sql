68f544ac72460e015583804b2543e720431421db
BRUVRY-LAGADEC <leo.brurvy.lagadec@ifremer.fr>
2024-11-13 16:17:37 +0100

    feat: :white_check_mark: dataset
    Populate database

--- java-demo/src/main/resources/schema.sql
+++ java-demo/src/main/resources/schema.sql
@@ -8,3 +8,8 @@ CREATE TABLE fruit (
      version INT,
      PRIMARY KEY (id)
 );
+
+INSERT INTO fruit (id, description, name, score, version)
+VALUES 
+(1, 'A round fruit', 'Apple', 'A', 1),
+(2, 'A yellow fruit', 'Banana', 'B', 1);
