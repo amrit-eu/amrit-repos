7602a151ccbaa1e44120f3df8cab9908f461b88e
Thomas G <thogar@noc.ac.uk>
2024-11-27 10:58:38 +0000

    Revert "testing with latest version"
    This reverts commit ce3839d91b3180e70574387d22d381fc2b2ea024.

--- typescript-demo/src/app/page.tsx
+++ typescript-demo/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by   editing <code>sr c/a pp/pag e.tsx</code>.
+            Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
