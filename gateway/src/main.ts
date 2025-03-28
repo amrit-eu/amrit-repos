000071b2ce044c3c9f6d43f6e6fce29eb5fbf22c
Yvanohe <yvanohe@gmail.com>
2025-03-28 16:49:06 +0100

    add global exceptions filter

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api')
+  const { httpAdapter} = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.setGlobalPrefix('api');
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
