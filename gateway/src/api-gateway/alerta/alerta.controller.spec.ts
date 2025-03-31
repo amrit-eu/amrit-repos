36e3b9d28aa88ab22cf4db72e6319760aecee3cc
Yvan Lubac <yvan.lubac@euro-argo.eu>
2025-03-31 10:01:01 +0200

    mock config service to provide env variable

--- gateway/src/api-gateway/alerta/alerta.controller.spec.ts
+++ gateway/src/api-gateway/alerta/alerta.controller.spec.ts
@@ -3,20 +3,31 @@ import { AlertaController } from './alerta.controller';
 import { AlertaService } from './alerta.service';
 import {Request} from 'express';
 import { HttpModule } from '@nestjs/axios';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 describe('AlertaController', () => {
   let alertaController: AlertaController;
   let alertaService: AlertaService
 
+  // Mock ConfigService
+const mockConfigService = {
+  getOrThrow: jest.fn((key: string) => {
+    const mockConfig: Record<string, string> = {
+      ALERTA_HOST: 'http://mock-alerta-host',
+      ALERTA_READ_API_KEY: 'mock-alerta-key',
+    };
+    if (mockConfig[key]) return mockConfig[key];
+    throw new Error(`Config key ${key} not found`);
+  }),
+};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        HttpModule,
-        ConfigModule.forRoot({ isGlobal: true }),
+        HttpModule,        
       ],
       controllers: [AlertaController],
-      providers: [AlertaService]
+      providers: [AlertaService, { provide: ConfigService, useValue: mockConfigService }]
     }).compile();
 
     alertaController = module.get<AlertaController>(AlertaController);
--- gateway/src/api-gateway/alerta/alerta.service.spec.ts
+++ gateway/src/api-gateway/alerta/alerta.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlertaService } from './alerta.service';
 import { HttpModule } from '@nestjs/axios';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 describe('AlertaService', () => {
   let service: AlertaService;
 
+    // Mock ConfigService
+  const mockConfigService = {
+    getOrThrow: jest.fn((key: string) => {
+      const mockConfig: Record<string, string> = {
+        ALERTA_HOST: 'http://mock-alerta-host',
+        ALERTA_READ_API_KEY: 'mock-alerta-key',
+      };
+      if (mockConfig[key]) return mockConfig[key];
+      throw new Error(`Config key ${key} not found`);
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule, ConfigModule.forRoot({ isGlobal: true })],
-      providers: [AlertaService],
+      imports: [HttpModule],
+      providers: [AlertaService, { provide: ConfigService, useValue: mockConfigService }],
       
     }).compile();
 
