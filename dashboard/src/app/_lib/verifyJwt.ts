b0b189700a241ffe4ec32e9ceb7f5474b61fa407
Yvanohe <yvanohe@gmail.com>
2025-04-14 14:16:26 +0200

    update nestjs/common version because of vulnerability

--- dashboard/src/app/_lib/verifyJwt.ts
+++ dashboard/src/app/_lib/verifyJwt.ts
@@ -7,16 +7,17 @@ const JWKS = createRemoteJWKSet(new URL(AUTHSERVICE_API_BASE_URL+'/.well-known/j
 // But if we want to protect some front end route with user role for example, it is better to already plan a jwt verification here 
 export async function verifyJwt(token: string | undefined =""): Promise<JWTVerifyResult['payload'] | null> {
   try {
-
-    if (!token || typeof token !== 'string' || token.trim() === "") {
-      return null;
+    const safeToken = typeof token === 'string' ? token.trim() : '';
+    if (!safeToken) {
+      throw new Error('Invalid token');    
     }
     
     const { payload } = await jwtVerify(token, JWKS, {
       algorithms: ['RS256'],
     });
     return payload;
-  } catch (err) {    
-    return null;
+  } catch (err) {
+    throw new Error('Token verification failed');
+    
   }
 }
