fc8f24289e832e3e225cf0104245b9c208239ff0
Yvanohe <yvanohe@gmail.com>
2025-04-18 14:36:06 +0200

    ts lint, security and test corrections

--- dashboard/src/components/alerts/subscriptions/AddFilterModal.tsx
+++ dashboard/src/components/alerts/subscriptions/AddFilterModal.tsx
@@ -58,7 +58,7 @@ const AddFilterModal = ({ open, onClose, onConfirm }: AddFilterModalProps) => {
         options = await res.json();
       }
     } catch  {
-      console.error(`Failed to fetch options for ${type}.`);
+      console.error(`Failed to fetch options.`);
     }
 
     setFilterOptions(options);
