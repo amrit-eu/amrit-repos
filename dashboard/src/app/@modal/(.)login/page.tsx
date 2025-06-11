a64134f29d1a3819fe3ce462da38c8d0b893709f
Yvanohe <yvanohe@gmail.com>
2025-06-11 14:06:12 +0200

    tentative fix for ts-test

--- dashboard/src/app/@modal/(.)login/page.tsx
+++ dashboard/src/app/@modal/(.)login/page.tsx
@@ -3,8 +3,8 @@ import LoginForm from '@/components/layout/login/LoginForm'
 import RoutedModal from '@/components/shared/modals/RoutedModal'
 import React from 'react'
 
-const Login = async ({ searchParams }: { searchParams: { callbackUrl?: string } }) => {
-  const {callbackUrl} = searchParams;
+const Login = async ({ searchParams }: { searchParams: Promise<{ callbackUrl?: string }> }) => {
+  const {callbackUrl} = await searchParams;
   return (
     <RoutedModal backgroundTransparent={true}>
         <LoginForm callbackUrl={callbackUrl} />
