7c6b6a96c86b261643fe29ae6c52c05d4fade042
Yvanohe <yvanohe@gmail.com>
2025-07-03 14:50:32 +0200

    adding PROTOCAL env var

--- gateway/src/utils/proxy.routes.ts
+++ gateway/src/utils/proxy.routes.ts
@@ -5,6 +5,7 @@ export type ProxyRoute = {
     host: string;
     targetPath: string;
     authHeader?: string;
+    protocol : string
   
 }
 
@@ -20,11 +21,13 @@ export function createProxyRouteMap(config: ConfigService): ProxyRouteMap {
   return {
     'api/alerta': {
       host: config.getOrThrow<string>('ALERTA_HOST'),
+      protocol: config.getOrThrow<string>('ALERTA_PROTOCOL'),
       targetPath: '/api',
       authHeader: `Key ${config.getOrThrow<string>('ALERTA_READ_API_KEY')}`,
     },
     'api/oceanops': {
       host: config.getOrThrow<string>('OCEANOPS_HOST'),
+      protocol: config.getOrThrow<string>('OCEANOPS_PROTOCOL'),
       targetPath: '/api/data',
     }
   };
