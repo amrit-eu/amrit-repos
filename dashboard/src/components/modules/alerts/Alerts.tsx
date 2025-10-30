43bdde2926ae53985046a7b212e447bee1b2b220
Yvan Lubac <yvan.lubac@euro-argo.eu>
2025-10-30 17:21:44 +0100

    optimize server side filters fetch for Alerts and more resilient to APIs errors

--- dashboard/src/components/modules/alerts/Alerts.tsx
+++ dashboard/src/components/modules/alerts/Alerts.tsx
@@ -6,32 +6,36 @@ import { CountryAPIResponse, CountryOption, Session, TopicOption } from '@/types
 import { FiltersValuesMap } from '@/types/filters';
 import { handleCountryAPIJsonResponse } from '@/lib/utils/handleCountryAPIJsonResponse';
 
-
+// server side component fetching data for the filters list
 const Alerts = async () => {
   // get user info :
   const session:Session |null = await verifySession();
 
-  // fetch alerts status and severities count from Alerta API :
-  const counts = await getFromGateway<AlertsCount>(
-    '/alerta/alerts/count'
-  )
-  // fetch country list :
-  const countryData = await getFromGateway<CountryAPIResponse>('/data/countries');
+  // parallele fetch for alerts status and severities count from Alerta API and County list and topics from oceanops api
+  const [countsRes, countryRes, topicsRes, eventsRes] = await Promise.allSettled([
+    getFromGateway<AlertsCount>('/alerta/alerts/count'),
+    getFromGateway<CountryAPIResponse>('/data/countries'),
+    getFromGateway<TopicOption[]>('/data/topics'),
+    getFromGateway<EventsCount>('/alerta/alerts/events'),
+  ]);
+
+  // retrieve values if no errors during fetch :
+  //  alerts counts
+  const counts = (countsRes.status === 'fulfilled' && countsRes.value) ? countsRes.value : { severityCounts: {}, statusCounts: {} } as AlertsCount
+  // country list :
+  const countryData = (countryRes.status === 'fulfilled' && countryRes.value) ? countryRes.value : {data:[]}
   const fetchedCountryOptions : CountryOption[]  = handleCountryAPIJsonResponse ( countryData);
   const sortedCountryOption = fetchedCountryOptions.sort((a, b) => a.name.localeCompare(b.name));
+  // topics list :
+  const topicsData = (topicsRes.status === 'fulfilled' && topicsRes.value) ? topicsRes.value : []
+  // event list :
+  const eventsCount = (eventsRes.status === 'fulfilled' && eventsRes.value) ? eventsRes.value : {events:[], status:null, total:0 }
 
-  // fetch topics list :
-  const topicsData = await getFromGateway<TopicOption[]>('/data/topics');
-
-  // fetch event list
-  const eventsCount = await getFromGateway<EventsCount>('/alerta/alerts/events');
-
-
-  // Fetch filters data server side
+  // set the filters values list
   const filtersValues :FiltersValuesMap = {
-    severity: Object.entries(counts.severityCounts).map(([key])=> `${key}`),
-    status:  Object.entries(counts.statusCounts).map(([key])=> `${key}`),
-    event: eventsCount.events.map(e => e.event),    
+    severity: Object.keys(counts?.severityCounts ?? {}),
+    status:  Object.keys(counts?.statusCounts ?? {}),
+    event: (eventsCount?.events ?? []).map(e => e?.event).filter(Boolean),    
     Country: sortedCountryOption,
     alert_category: topicsData
   }
