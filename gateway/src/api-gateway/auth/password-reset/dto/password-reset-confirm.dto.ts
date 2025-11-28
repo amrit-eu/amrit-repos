1b58ba3d9930cee5739013cd55301011ba606b66
Yvan Lubac <yvan.lubac@euro-argo.eu>
2025-11-28 14:08:21 +0100

    fix password rules for sonarqube

--- gateway/src/api-gateway/auth/password-reset/dto/password-reset-confirm.dto.ts
+++ gateway/src/api-gateway/auth/password-reset/dto/password-reset-confirm.dto.ts
@@ -1,9 +1,11 @@
 import { IsString, MinLength, Matches } from 'class-validator';
 export class PasswordResetConfirmDto {
   @IsString() token!: string;
-
+ 
   @MinLength(12)
-  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)/,
-    { message: 'Weak password' })
+  @Matches(/[a-z]/, { message: 'Must contain a lowercase letter' })
+  @Matches(/[A-Z]/, { message: 'Must contain an uppercase letter' })
+  @Matches(/\d/, { message: 'Must contain a digit' })
+  @Matches(/\W/, { message: 'Must contain a special character' })
   newPassword!: string;
 }
